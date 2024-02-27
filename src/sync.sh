77307fe86c34b057616fb836d71bf372827161bd
Micah Nagel <micah.nagel@defenseunicorns.com>
2024-02-27 13:42:19 -0700

    wip: file location

--- src/sync.sh
+++ src/sync.sh
@@ -4,7 +4,7 @@ echo "Syncing customizations to Keycloak"
 
 # Ensure the import directory exists
 mkdir -p /opt/keycloak/data/import/
-mkdir -p /opt/keycloak/truststore/
+mkdir -p /opt/keycloak/conf/
 
 # Copy the files to their respective directories
 cp -fvu realm.json /opt/keycloak/data/import/realm.json
@@ -12,9 +12,9 @@ cp -fvur theme /opt/keycloak/themes/theme
 cp -fvu *.jar /opt/keycloak/providers/
 # Wrap in some conditional logic based on an ENV, copy a different truststore
 if [ -z "${CUSTOM_TRUSTSTORE}" ]; then
-  cp -fvi "$CUSTOM_TRUSTSTORE" /opt/keycloak/truststore/truststore.jks
+  cp -fvi "$CUSTOM_TRUSTSTORE" /opt/keycloak/conf/truststore.jks
 else
-  cp -fvu truststore.jks /opt/keycloak/truststore/truststore.jks
+  cp -fvu truststore.jks /opt/keycloak/conf/truststore.jks
 fi
 
 echo "Sync complete"
