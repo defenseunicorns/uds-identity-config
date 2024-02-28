5993ee25cc584aa15aed0d8206418cab94ab4928
Megamind <882485+jeff-mccoy@users.noreply.github.com>
2024-02-28 16:22:18 -0600

    feat: generalize plugin & truststore customization (#7)

---
+++ src/extra-jars/README.md
@@ -0,0 +1,1 @@
+Add any compiled JARs to this directory and they will be included during Keycloak startup.
--- src/truststore/ca-to-jks.sh
+++ src/truststore/ca-to-jks.sh
@@ -1,11 +1,14 @@
 #!/bin/bash
+set -e
 
-### Grab DoD CAs
-X509_CA_BUNDLE="${PWD}/dod_cas.pem"
+# Download DoD CA Certs
+X509_CA_BUNDLE="$(pwd)/authorized_certs.pem"
 
-unzip -q -d /tmp/dod_cas /tmp/dod_cas/dod_cas.zip
+# Extract the archive
+unzip -q -d /tmp/authorized_certs /tmp/authorized_certs/authorized_certs.zip
 
-find /tmp/dod_cas -name '*.cer' -print0 |
+# Convert all certs to PEM format and remove extra lines
+find /tmp/authorized_certs -name '*.cer' -print0 |
 while IFS= read -r -d '' cert; do
   if openssl x509 -inform der -in "$cert" -noout 2>/dev/null; then
     echo "Found in Der format: $cert Converting to PEM"
@@ -17,39 +20,28 @@ while IFS= read -r -d '' cert; do
   fi
 done
 
-find /tmp/dod_cas -type f  -iname '*.cer' -a ! -regex '\(.*EMAIL.*\|.*SW.*\)' -printf "\n" -exec cat {} \; > /tmp/dod_cas/cas.pem
-cp /tmp/dod_cas/cas.pem ${X509_CA_BUNDLE}
+# Combine all certs into a single file, excluding email and software certs
+find /tmp/authorized_certs -type f  -iname '*.cer' -a ! -regex "${CA_REGEX_EXCLUSION_FILTER}" -printf "\n" -exec cat {} \; > ${X509_CA_BUNDLE}
 
-### Build out Truststore
-MAIN_DIR=`pwd`
-X509_CRT_DELIMITER="/-----BEGIN CERTIFICATE-----/"
-JKS_TRUSTSTORE_FILE="truststore.jks"
-JKS_TRUSTSTORE_PATH="${MAIN_DIR}/${JKS_TRUSTSTORE_FILE}"
+# Create a truststore and import the certs
+JKS_TRUSTSTORE_PATH="$(pwd)/truststore.jks"
+# Using the Keycloak default because we are only storing public certs
 TRUSTSTORE_PASSWORD="password"
-TEMPORARY_CERTIFICATE="temporary_ca.crt"
 
 pushd /tmp >& /dev/null
 
-echo "Creating truststore ${JKS_TRUSTSTORE_PATH}"
-cat "${X509_CA_BUNDLE}" > ${TEMPORARY_CERTIFICATE}
-csplit -s -z -f crt- "${TEMPORARY_CERTIFICATE}" "${X509_CRT_DELIMITER}" '{*}'
+csplit -s -z -f crt- "${X509_CA_BUNDLE}" "/-----BEGIN CERTIFICATE-----/" '{*}'
 for CERT_FILE in crt-*; do
   # Validate cert is not expired
   if openssl x509 -checkend 0 -noout -in $CERT_FILE &> /dev/null; then
+    echo "Adding $CERT_FILE to truststore"
     keytool -import -noprompt -keystore "${JKS_TRUSTSTORE_PATH}" -file "${CERT_FILE}" -storepass "${TRUSTSTORE_PASSWORD}" -alias "service-${CERT_FILE}" >& /dev/null
   fi
 done
-if [ -f "${JKS_TRUSTSTORE_PATH}" ]; then
-  echo "Truststore successfully created at: ${JKS_TRUSTSTORE_PATH}"
-else
-  echo "ERROR: Creating truststore: ${JKS_TRUSTSTORE_PATH}"
-  exit 1
-fi
 
 popd >& /dev/null
 
-echo "Validating truststore"
-keytool -list -keystore $JKS_TRUSTSTORE_PATH -storepass "${TRUSTSTORE_PASSWORD}" >& /dev/null
+keytool -list -keystore $JKS_TRUSTSTORE_PATH -storepass "${TRUSTSTORE_PASSWORD}"
 if [ $? == 0 ]; then
   echo "Truststore validated"
 else
