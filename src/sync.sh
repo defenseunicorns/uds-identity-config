0007ec15fb177f99703e5d728bdcd2eb37c0fc5d
Rob Ferguson <rjferguson21@gmail.com>
2024-03-18 12:33:26 -0500

    fix: register form id robot check (#34)

--- src/sync.sh
+++ src/sync.sh
@@ -6,10 +6,11 @@ echo "Syncing customizations to Keycloak"
 # Ensure the import directory exists
 mkdir -p /opt/keycloak/data/import/
 mkdir -p /opt/keycloak/conf/
+mkdir -p /opt/keycloak/themes/theme/
 
 # Copy the files to their respective directories
 cp -fvu realm.json /opt/keycloak/data/import/realm.json
-cp -fvur theme /opt/keycloak/themes/theme
+cp -fvur theme/* /opt/keycloak/themes/theme/
 cp -fvu *.jar /opt/keycloak/providers/
 cp -fvu truststore.jks /opt/keycloak/conf/truststore.jks
 
