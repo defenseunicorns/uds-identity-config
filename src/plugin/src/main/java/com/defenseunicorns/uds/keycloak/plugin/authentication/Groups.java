9720cd391de108cfb9e59f8536da75b67a9e70bf
UnicornChance <chance@defenseunicorns.com>
2024-06-28 10:56:25 -0600

    chore: cleanup imports

--- src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/authentication/Groups.java
+++ src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/authentication/Groups.java
@@ -1,8 +1,6 @@
 package com.defenseunicorns.uds.keycloak.plugin.authentication;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
-import com.fasterxml.jackson.databind.JsonNode;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class Groups {
     @JsonProperty("anyOf")
