cc69e4ea4e5a1fb41eea5ef9e875397ea0a1a724
UnicornChance <chance@defenseunicorns.com>
2024-10-15 14:58:36 -0600

    chore: update license headers

--- src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/eventListeners/RegisterEventListenerProvider.java
+++ src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/eventListeners/RegisterEventListenerProvider.java
@@ -1,3 +1,8 @@
+/*
+ * Copyright 2024 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 package com.defenseunicorns.uds.keycloak.plugin.eventListeners;
 
 import java.nio.charset.StandardCharsets;
--- src/test/cypress/e2e/registration.cy.ts
+++ src/test/cypress/e2e/registration.cy.ts
@@ -1,3 +1,8 @@
+/**
+ * Copyright 2024 Defense Unicorns
+ * SPDX-License-Identifier: AGPL-3.0-or-later OR LicenseRef-Defense-Unicorns-Commercial
+ */
+
 import { RegistrationFormData } from "../support/types";
 
 describe("CAC Registration Flow", () => {
