70cdb8a20ebcfc977a5036f135f0555f2d2524ab
Sebastian Łaskawiec <sebastian.laskawiec@gmail.com>
2025-03-27 21:43:54 +0100

    feat: pepr Keycloak Client management (#358)
    * UDS Operator to use Client Credentials for Client management
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
    
    * Fix tests
    
    * CI debugging
    
    * Fix NPE
    
    * Client Credentials docs
    
    * Comments addressed
    
    * Comments addressed
    
    * Fix test
    
    * Mode debug logging
    
    * Version upgrade
    
    * Update src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/clientpolicy/executor/UDSClientPolicyPermissionsExecutorFactory.java
    
    Co-authored-by: Chance <139784371+UnicornChance@users.noreply.github.com>
    
    * Comments addressed
    
    * Comments addressed
    
    * Update docs/reference/UDS Core/IdAM/upgrading-versions.md
    
    Co-authored-by: Micah Nagel <micah.nagel@defenseunicorns.com>
    
    * Comments addressed
    
    * fix: revert custom branch for main branch
    
    ---------
    
    Signed-off-by: Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
    Co-authored-by: Chance <139784371+UnicornChance@users.noreply.github.com>
    Co-authored-by: Micah Nagel <micah.nagel@defenseunicorns.com>
    Co-authored-by: UnicornChance <chance@defenseunicorns.com>

--- src/test/cypress/support/commands.ts
+++ src/test/cypress/support/commands.ts
@@ -124,3 +124,36 @@ Cypress.Commands.add("avoidX509", () => {
     }
   });
 })
+
+Cypress.Commands.add("getAccessToken", () => {
+  return cy.exec('uds zarf tools kubectl get secret keycloak-client-secrets -n keycloak -o jsonpath="{.data.uds-operator}"').then((result) => {
+    expect(result.code).to.eq(0);
+    expect(result.stdout).not.contains(" ");
+
+    const clientSecret = Buffer.from(result.stdout, 'base64').toString('utf-8');
+
+    return cy.request({
+      method: "POST",
+      url: "https://keycloak.admin.uds.dev/realms/uds/protocol/openid-connect/token",
+      headers: {
+        "Content-Type": "application/x-www-form-urlencoded",
+      },
+      body: {
+        client_id: "uds-operator",
+        client_secret: `${clientSecret}`,
+        grant_type: "client_credentials",
+      },
+    }).then((response) => {
+      expect(response.status).to.eq(200);
+      const accessToken = response.body.access_token;
+      expect(accessToken).to.be.a("string");
+
+      const tokenParts = accessToken.split('.');
+      const tokenPayload = Buffer.from(tokenParts[1], 'base64').toString('utf-8');
+
+      expect(tokenPayload).contains("manage-clients");
+
+      return accessToken;
+    });
+  });
+});
