130652df46cf2ec1c6edc2c1681be05b9e1e0f0a
Sebastian Łaskawiec <sebastian.laskawiec@defenseunicorns.com>
2025-04-15 11:23:25 +0200

    Client Policy Security Hardening

--- src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/clientpolicy/executor/UDSClientPolicyPermissionsExecutor.java
+++ src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/clientpolicy/executor/UDSClientPolicyPermissionsExecutor.java
@@ -5,9 +5,18 @@
 
 package com.defenseunicorns.uds.keycloak.plugin.clientpolicy.executor;
 
+import com.defenseunicorns.uds.keycloak.plugin.CustomAWSSAMLGroupMapper;
+import com.defenseunicorns.uds.keycloak.plugin.CustomGroupPathMapper;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.keycloak.events.Errors;
 import org.keycloak.models.ClientModel;
-import org.keycloak.representations.idm.ClientPolicyExecutorConfigurationRepresentation;
+import org.keycloak.models.ClientScopeModel;
+import org.keycloak.models.KeycloakSession;
+import org.keycloak.protocol.ProtocolMapper;
+import org.keycloak.protocol.oidc.mappers.*;
+import org.keycloak.protocol.saml.mappers.RoleListMapper;
+import org.keycloak.protocol.saml.mappers.UserAttributeStatementMapper;
+import org.keycloak.protocol.saml.mappers.UserPropertyAttributeStatementMapper;
 import org.keycloak.representations.idm.ClientRepresentation;
 import org.keycloak.services.clientpolicy.ClientPolicyContext;
 import org.keycloak.services.clientpolicy.ClientPolicyException;
@@ -18,6 +27,9 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.lang.invoke.MethodHandles;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Keycloak Client Policy Executor for the UDS Operator.
@@ -26,26 +38,69 @@ import java.lang.invoke.MethodHandles;
  * * The Client is marked as created by the UDS Operator by setting the attribute "created-by=uds-operator".
  * * The UDS Operator's Token is checked if it can access particular Client (the client need to contain "created-by=uds-operator" attribute).
  * * The Client can't use the Full Scope Allowed feature.
- * * The Client can use all potential Client Scopes. This will be restricted in the future and is tracked with https://github.com/defenseunicorns/uds-identity-config/issues/385
+ * * The Client can only use the Protocol Mappers that are whitelisted in the configuration. The default, opinionated configuration uses OIDC nad UDS Protocol Mappers.
+ * * The Client can only use the Custom Client Scopes that are whitelisted in the configuration. The default, opinionated configuration uses UDS Provided Client Scopes, Client Scopes configured at the Realm level as well as SAML defaults.
  */
-public class UDSClientPolicyPermissionsExecutor implements ClientPolicyExecutorProvider<ClientPolicyExecutorConfigurationRepresentation> {
+public class UDSClientPolicyPermissionsExecutor implements ClientPolicyExecutorProvider<UDSClientPolicyPermissionsExecutorConfiguration> {
 
     public static final String UDS_OPERATOR_CLIENT_ID = "uds-operator";
     public static final String ATTRIBUTE_UDS_OPERATOR = "created-by";
     public static final String ATTRIBUTE_UDS_OPERATOR_VALUE = "uds-operator";
 
-    private final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+    public static final List<String> DEFAULT_ALLOWED_PROTOCOL_MAPPERS = List.of(
+            UserAttributeStatementMapper.PROVIDER_ID,
+            UserAttributeMapper.PROVIDER_ID,
+            UserPropertyAttributeStatementMapper.PROVIDER_ID,
+            UserPropertyMapper.PROVIDER_ID,
+            FullNameMapper.PROVIDER_ID,
+            AddressMapper.PROVIDER_ID,
+            new SHA256PairwiseSubMapper().getId(),
+            RoleListMapper.PROVIDER_ID,
+            CustomAWSSAMLGroupMapper.PROVIDER_ID
+    );
+
+    public static final List<String> DEFAULT_ALLOWED_CLIENT_SCOPES = List.of(
+            "openid",
+            "aws-groups",
+            CustomGroupPathMapper.GROUPS_CLAIM,
+            CustomGroupPathMapper.BARE_GROUPS_CLAIM,
+            "mapper-saml-username-name",
+            "mapper-saml-email-email",
+            "mapper-saml-username-login",
+            "mapper-saml-firstname-first_name",
+            "mapper-saml-lastname-last_name",
+            "mapper-saml-grouplist-groups",
+            "mapper-oidc-username-username",
+            "mapper-oidc-mattermostid-id",
+            "mapper-oidc-email-email"
+    );
+
+    protected final static Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+    protected final KeycloakSession keycloakSession;
+    protected UDSClientPolicyPermissionsExecutorConfiguration configuration;
+
+    public UDSClientPolicyPermissionsExecutor(KeycloakSession session) {
+        this.keycloakSession = session;
+    }
+
+    private static String toString(ClientRepresentation rep) {
+        try {
+            return new ObjectMapper().writeValueAsString(rep);
+        } catch (Exception e) {
+            return "Failed to convert ClientRepresentation to JSON, " + e.getMessage();
+        }
+    }
 
     @Override
     public void executeOnEvent(ClientPolicyContext context) throws ClientPolicyException {
         if ((context instanceof ClientCRUDContext clientCRUDContext)) {
-
             logger.debug("Executing UDSClientPolicyPermissionsExecutor, authenticated to Client ID: {}, ", getAuthenticatedClientId(clientCRUDContext));
 
             if (clientCRUDContext.getAuthenticatedClient() != null && UDS_OPERATOR_CLIENT_ID.equals(getAuthenticatedClientId(clientCRUDContext))) {
                 switch (context.getEvent()) {
                     case UPDATE:
-                        logger.debug("Updating existing Client with Client ID: {}", clientCRUDContext.getTargetClient().getClientId());
+                        logger.trace("Updating existing Client with Client ID: {}", clientCRUDContext.getTargetClient().getClientId());
                         if (!isOwnedByUDSOperator(clientCRUDContext.getTargetClient())) {
                             throw new ClientPolicyException(Errors.UNAUTHORIZED_CLIENT, "The Client doesn't have the " + ATTRIBUTE_UDS_OPERATOR + "=" + ATTRIBUTE_UDS_OPERATOR_VALUE + " attribute. Rejecting request.");
                         }
@@ -53,13 +108,13 @@ public class UDSClientPolicyPermissionsExecutor implements ClientPolicyExecutorP
                         break;
 
                     case REGISTER:
-                        logger.debug("Creating new Client with Client ID: {}", clientCRUDContext.getProposedClientRepresentation().getClientId());
+                        logger.trace("Creating new Client with Client ID: {}", clientCRUDContext.getProposedClientRepresentation().getClientId());
                         enforceClientSettings(clientCRUDContext.getProposedClientRepresentation());
                         break;
 
                     case VIEW:
                     case UNREGISTER:
-                        logger.debug("Viewing or deleting Client with Client ID: {}", clientCRUDContext.getTargetClient().getClientId());
+                        logger.trace("Viewing or deleting Client with Client ID: {}", clientCRUDContext.getTargetClient().getClientId());
                         if (!isOwnedByUDSOperator(clientCRUDContext.getTargetClient())) {
                             throw new ClientPolicyException(Errors.UNAUTHORIZED_CLIENT, "The Client doesn't have the " + ATTRIBUTE_UDS_OPERATOR + "=" + ATTRIBUTE_UDS_OPERATOR_VALUE + " attribute. Rejecting request.");
                         }
@@ -82,13 +137,58 @@ public class UDSClientPolicyPermissionsExecutor implements ClientPolicyExecutorP
         return ATTRIBUTE_UDS_OPERATOR_VALUE.equals(client.getAttribute(ATTRIBUTE_UDS_OPERATOR));
     }
 
-    @Override
-    public void setupConfiguration(ClientPolicyExecutorConfigurationRepresentation config) {
+    void setAllowedProtocolMappers(ClientRepresentation rep) {
+        if (rep.getProtocolMappers() != null && !rep.getProtocolMappers().isEmpty()) {
+            rep.getProtocolMappers()
+                    .removeIf(mapper -> !configuration.getAllowedProtocolMappers().contains(mapper.getProtocolMapper()));
+        }
+    }
+
+    void setAllowedCustomClientScopes(ClientRepresentation rep) {
+        if (rep.getDefaultClientScopes() != null && !rep.getDefaultClientScopes().isEmpty()) {
+            rep.getDefaultClientScopes()
+                    .removeIf(scope -> !configuration.getAllowedClientScopes().contains(scope));
+        }
+        if (rep.getOptionalClientScopes() != null && !rep.getOptionalClientScopes().isEmpty()) {
+            rep.getOptionalClientScopes()
+                    .removeIf(scope -> !configuration.getAllowedClientScopes().contains(scope));
+        }
     }
 
     @Override
-    public Class<ClientPolicyExecutorConfigurationRepresentation> getExecutorConfigurationClass() {
-        return ClientPolicyExecutorConfigurationRepresentation.class;
+    public void setupConfiguration(UDSClientPolicyPermissionsExecutorConfiguration config) {
+        if (config.isUseDefaultAllowedProtocolMappers()) {
+            List<String> defenseUnicornsProtocolMappers = this.keycloakSession.getKeycloakSessionFactory().
+                    getProviderFactoriesStream(ProtocolMapper.class)
+                    .filter(o -> o.getClass().getPackageName().startsWith("com.defenseunicorns.uds.keycloak"))
+                    .map(o -> o.getId())
+                    .toList();
+
+            List<String> allowedProtocolMappers = new ArrayList<>();
+            allowedProtocolMappers.addAll(DEFAULT_ALLOWED_PROTOCOL_MAPPERS);
+            allowedProtocolMappers.addAll(defenseUnicornsProtocolMappers);
+            if (config.getAllowedProtocolMappers() != null)
+                allowedProtocolMappers.addAll(config.getAllowedProtocolMappers());
+            config.setAllowedProtocolMappers(allowedProtocolMappers);
+        }
+
+        if (config.isUseDefaultAllowedClientScopes()) {
+            List<String> allowedClientScopes = new ArrayList<>();
+            allowedClientScopes.addAll(DEFAULT_ALLOWED_CLIENT_SCOPES);
+            // Add Realm defaults
+            allowedClientScopes.addAll(this.keycloakSession.getContext().getRealm().getDefaultClientScopesStream(true).map(ClientScopeModel::getName).collect(Collectors.toList()));
+            if (config.getAllowedClientScopes() != null)
+                allowedClientScopes.addAll(config.getAllowedClientScopes());
+            config.setAllowedClientScopes(allowedClientScopes);
+        }
+
+        logger.trace("Initializing with configuration: {}", config);
+        this.configuration = config;
+    }
+
+    @Override
+    public Class<UDSClientPolicyPermissionsExecutorConfiguration> getExecutorConfigurationClass() {
+        return UDSClientPolicyPermissionsExecutorConfiguration.class;
     }
 
     @Override
@@ -97,8 +197,14 @@ public class UDSClientPolicyPermissionsExecutor implements ClientPolicyExecutorP
     }
 
     void enforceClientSettings(ClientRepresentation rep) {
+        logger.trace("Client Representation before enforcements: {}", toString(rep));
+
         setUDSOperatorAsOwner(rep);
         setFullScopeDisabled(rep);
+        setAllowedProtocolMappers(rep);
+        setAllowedCustomClientScopes(rep);
+
+        logger.trace("Client Representation after enforcements: {}", toString(rep));
     }
 
     private void setUDSOperatorAsOwner(ClientRepresentation rep) {
--- src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/clientpolicy/executor/UDSClientPolicyPermissionsExecutorFactory.java
+++ src/plugin/src/main/java/com/defenseunicorns/uds/keycloak/plugin/clientpolicy/executor/UDSClientPolicyPermissionsExecutorFactory.java
@@ -8,22 +8,25 @@ package com.defenseunicorns.uds.keycloak.plugin.clientpolicy.executor;
 import org.keycloak.Config;
 import org.keycloak.models.KeycloakSession;
 import org.keycloak.models.KeycloakSessionFactory;
+import org.keycloak.protocol.ProtocolMapper;
 import org.keycloak.provider.ProviderConfigProperty;
+import org.keycloak.provider.ProviderFactory;
 import org.keycloak.services.clientpolicy.executor.ClientPolicyExecutorProviderFactory;
 
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 /**
  * Factory for creating and registering the UDS Client Policy Executor with Keycloak.
- *
+ * <p>
  * This class is needed to integrate the custom UDS security hardening logic into Keycloak's
  * client management process. It creates instances of UDSClientPolicyPermissionsExecutor, which
  * enforce policies such as:
  * - Marking clients as created by the UDS Operator by setting the "created-by=uds-operator" attribute.
  * - Restricting operations on clients not owned by the UDS Operator or not following the naming convention.
  * - Disabling the "Full Scope Allowed" feature and limiting client scopes to an allowed set.
- *
+ * <p>
  * Without this factory, Keycloak would not be able to instantiate and manage the custom policy
  * executor, and the UDS-specific client policies would not be enforced.
  */
@@ -31,9 +34,52 @@ public class UDSClientPolicyPermissionsExecutorFactory implements ClientPolicyEx
 
     public static final String PROVIDER_ID = "uds-operator-permissions";
 
+    public static final String ADDITIONAL_ALLOWED_PROTOCOL_MAPPER_TYPES = "allowed-protocol-mappers";
+    public static final String ADDITIONAL_ALLOWED_CLIENT_SCOPES = "allowed-client-scopes";
+    public static final String USE_DEFAULT_ALLOWED_PROTOCOL_MAPPER_TYPES = "use-default-allowed-protocol-mappers";
+    public static final String USE_DEFAULT_ALLOWED_CLIENT_SCOPES = "use-default-allowed-client-scopes";
+
+    protected static final List<ProviderConfigProperty> configProperties = new ArrayList<>();
+
+    ProviderConfigProperty additionalAllowedProtocolMappers = new ProviderConfigProperty();
+    ProviderConfigProperty useDefaultAllowedProtocolMappers = new ProviderConfigProperty();
+    ProviderConfigProperty additionalAllowedClientScopes = new ProviderConfigProperty();
+    ProviderConfigProperty useDefaultAllowedClientScopes = new ProviderConfigProperty();
+
+    {
+        additionalAllowedProtocolMappers.setName(ADDITIONAL_ALLOWED_PROTOCOL_MAPPER_TYPES);
+        additionalAllowedProtocolMappers.setLabel("Additional Allowed Protocol Mappers");
+        additionalAllowedProtocolMappers.setHelpText("Additional Allowed Protocol Mappers");
+        additionalAllowedProtocolMappers.setType(ProviderConfigProperty.MULTIVALUED_LIST_TYPE);
+        configProperties.add(additionalAllowedProtocolMappers);
+
+        useDefaultAllowedProtocolMappers.setName(USE_DEFAULT_ALLOWED_PROTOCOL_MAPPER_TYPES);
+        useDefaultAllowedProtocolMappers.setLabel("Use UDS Default Allowed Protocol Mappers");
+        useDefaultAllowedProtocolMappers.setHelpText("Setting this to true results in default OIDC and UDS Protocol Mappers to be whitelisted");
+        useDefaultAllowedProtocolMappers.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        useDefaultAllowedProtocolMappers.setDefaultValue(Boolean.TRUE.toString());
+        configProperties.add(useDefaultAllowedProtocolMappers);
+
+        additionalAllowedClientScopes.setName(ADDITIONAL_ALLOWED_CLIENT_SCOPES);
+        additionalAllowedClientScopes.setLabel("Additional Allowed Client Scopes");
+        additionalAllowedClientScopes.setHelpText("Additional Allowed Client Scopes");
+        additionalAllowedClientScopes.setType(ProviderConfigProperty.MULTIVALUED_STRING_TYPE);
+        // Unfortunately, when we're creating a Client Policy we don't have access to Client Scopes yet.
+        // This is different in
+//            additionalAllowedClientScopes.setOptions(getClientScopes());
+        configProperties.add(additionalAllowedClientScopes);
+
+        useDefaultAllowedClientScopes.setName(USE_DEFAULT_ALLOWED_CLIENT_SCOPES);
+        useDefaultAllowedClientScopes.setLabel("Use UDS Default Client Scopes");
+        useDefaultAllowedClientScopes.setHelpText("Setting this to true results in default OIDC and UDS Client Scopes to be whitelisted");
+        useDefaultAllowedClientScopes.setType(ProviderConfigProperty.BOOLEAN_TYPE);
+        useDefaultAllowedClientScopes.setDefaultValue(Boolean.TRUE.toString());
+        configProperties.add(useDefaultAllowedClientScopes);
+    }
+
     @Override
     public UDSClientPolicyPermissionsExecutor create(KeycloakSession session) {
-        return new UDSClientPolicyPermissionsExecutor();
+        return new UDSClientPolicyPermissionsExecutor(session);
     }
 
     @Override
@@ -41,7 +87,16 @@ public class UDSClientPolicyPermissionsExecutorFactory implements ClientPolicyEx
     }
 
     @Override
-    public void postInit(KeycloakSessionFactory factory) {
+    public synchronized void postInit(KeycloakSessionFactory factory) {
+        // Factory init is tricky and might be called multiple times.
+        // We initialize everything at the instance construction time and keep updating the options.
+        additionalAllowedProtocolMappers.setOptions(getProtocolMapperFactoryIds(factory));
+    }
+
+    private List<String> getProtocolMapperFactoryIds(KeycloakSessionFactory sessionFactory) {
+        return sessionFactory.getProviderFactoriesStream(ProtocolMapper.class)
+                .map(ProviderFactory::getId)
+                .collect(Collectors.toList());
     }
 
     @Override
@@ -60,6 +115,9 @@ public class UDSClientPolicyPermissionsExecutorFactory implements ClientPolicyEx
 
     @Override
     public List<ProviderConfigProperty> getConfigProperties() {
-        return Collections.emptyList();
+        return List.of(additionalAllowedProtocolMappers,
+                useDefaultAllowedProtocolMappers,
+                additionalAllowedClientScopes,
+                useDefaultAllowedClientScopes);
     }
 }
