e69f1d4db60b2b02542ddb5744660dd2e6b62a69
Chance <139784371+UnicornChance@users.noreply.github.com>
2025-05-05 12:52:22 -0600

    fix: remove dynamic client registration (#441)
    * fix: remove dynamic clients
    
    * fix: use core branch
    
    * fix: my stupidity
    
    * add dcr test back in and trusted hosts
    
    * chore: add upgrade steps for trusted hosts
    
    * fix: add client registration limit
    
    * fix: pr comments
    
    * fix: remove other trustedhost

--- src/test/cypress/e2e/client-credentials.cy.ts
+++ src/test/cypress/e2e/client-credentials.cy.ts
@@ -4,117 +4,133 @@
  */
 
 describe("UDS Operator Client Credentials", () => {
-    if (Cypress.env("PEPR_KEYCLOAK_CLIENT_STRATEGY") !== "dynamic_client_registration") {
-        it ("UDS Operator can obtain Access Token", () => {
-            cy.getAccessToken()
-        });
+    it("UDS Operator can obtain Access Token", () => {
+        cy.getAccessToken()
+    });
 
-        it("UDS Operator can delete its own Client", () => {
-            const randomClientId = `client-${Math.random().toString(36).substring(2, 15)}`;
+    it("UDS Operator can delete its own Client", () => {
+        const randomClientId = `client-${Math.random().toString(36).substring(2, 15)}`;
 
-            cy.getAccessToken().then((accessToken: string) => {
+        cy.getAccessToken().then((accessToken: string) => {
+            cy.request({
+                method: 'POST',
+                url: 'https://keycloak.admin.uds.dev/admin/realms/uds/clients',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+                body: {
+                    clientId: randomClientId,
+                    enabled: true,
+                    publicClient: true,
+                    redirectUris: ['http://localhost/*']
+                }
+            }).then((response) => {
+                expect(response.status).to.eq(201);
+                return response.headers['location'] as string;
+            }).then((clientUrl: string) => {
                 cy.request({
-                    method: 'POST',
-                    url: 'https://keycloak.admin.uds.dev/admin/realms/uds/clients',
+                    method: 'DELETE',
+                    url: clientUrl,
                     headers: {
                         'Authorization': `Bearer ${accessToken}`,
                         'Content-Type': 'application/json'
                     },
-                    body: {
-                        clientId: randomClientId,
-                        enabled: true,
-                        publicClient: true,
-                        redirectUris: ['http://localhost/*']
+                }).then((response) => {
+                    expect(response.status).to.eq(204);
+                });
+            });
+        });
+    });
+
+    it("UDS Operator can't delete a built-in Client", () => {
+        cy.getAccessToken().then((accessToken: string) => {
+            cy.request({
+                method: 'GET',
+                url: 'https://keycloak.admin.uds.dev/admin/realms/uds/clients',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                const brokerClient = response.body.find((client: any) => client.clientId === "broker");
+                const clientUrl = `https://keycloak.admin.uds.dev/admin/realms/uds/clients/${brokerClient.id}`;
+                cy.request({
+                    failOnStatusCode: false,
+                    method: 'DELETE',
+                    url: clientUrl,
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json'
+                    },
+                }).then((response) => {
+                    expect(response.status).to.eq(400);
+                    expect(response.body).to.deep.equal({
+                        error: "unauthorized_client",
+                        error_description: "The Client doesn't have the created-by=uds-operator attribute. Rejecting request."
+                    });
+                });
+            });
+        });
+    });
+
+    it("UDSClientPolicyPermissionsExecutor removes non-whitelisted mappers and claims", () => {
+        cy.exec("uds zarf tools kubectl apply -f ./resources/test-package.yaml").its('code').should('eq', 0);
+        cy.exec("uds zarf tools kubectl wait --for=condition=Ready package/test-package -n test-package --timeout=300s").its('code').should('eq', 0);
+
+        cy.getAccessToken().then((accessToken: string) => {
+            cy.request({
+                method: 'GET',
+                url: 'https://keycloak.admin.uds.dev/admin/realms/uds/clients',
+                headers: {
+                    'Authorization': `Bearer ${accessToken}`,
+                    'Content-Type': 'application/json'
+                },
+            }).then((response) => {
+                expect(response.status).to.eq(200);
+                const testClient = response.body.find((client: any) => client.clientId === "uds-core-admin-test");
+                const protocolMappersUrl = `https://keycloak.admin.uds.dev/admin/realms/uds/clients/${testClient.id}/protocol-mappers/models`;
+                cy.request({
+                    method: 'GET',
+                    url: protocolMappersUrl,
+                    headers: {
+                        'Authorization': `Bearer ${accessToken}`,
+                        'Content-Type': 'application/json'
+                    },
+                }).then((response) => {
+                    expect(response.status).to.eq(200);
+
+                    interface ProtocolMapper {
+                        name: string;
                     }
-                }).then((response) => {
-                    expect(response.status).to.eq(201);
-                    return response.headers['location'] as string;
-                }).then((clientUrl: string) => {
-                    cy.request({
-                        method: 'DELETE',
-                        url: clientUrl,
-                        headers: {
-                            'Authorization': `Bearer ${accessToken}`,
-                            'Content-Type': 'application/json'
-                        },
-                    }).then((response) => {
-                        expect(response.status).to.eq(204);
-                    });
+                    const protocolMappers = response.body as ProtocolMapper[];
+                    expect(protocolMappers).to.have.length(3);
+                    expect(protocolMappers.map((mapper) => mapper.name)).to.include.members([
+                        "valid-protocol-mapper-1",
+                        "valid-protocol-mapper-2",
+                        "valid-protocol-mapper-3",
+                    ]);
                 });
             });
         });
+    });
 
-        it("UDS Operator can't delete a built-in Client", () => {
-            cy.getAccessToken().then((accessToken: string) => {
-                cy.request({
-                    method: 'GET',
-                    url: 'https://keycloak.admin.uds.dev/admin/realms/uds/clients',
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`,
-                        'Content-Type': 'application/json'
-                    },
-                }).then((response) => {
-                    expect(response.status).to.eq(200);
-                    const brokerClient = response.body.find((client: any) => client.clientId === "broker");
-                    const clientUrl = `https://keycloak.admin.uds.dev/admin/realms/uds/clients/${brokerClient.id}`;
-                    cy.request({
-                        failOnStatusCode: false,
-                        method: 'DELETE',
-                        url: clientUrl,
-                        headers: {
-                            'Authorization': `Bearer ${accessToken}`,
-                            'Content-Type': 'application/json'
-                        },
-                    }).then((response) => {
-                        expect(response.status).to.eq(400);
-                        expect(response.body).to.deep.equal({
-                            error: "unauthorized_client",
-                            error_description: "The Client doesn't have the created-by=uds-operator attribute. Rejecting request."
-                        });
-                    });
-                });
-            });
+    it("Dynamic Client Registration is disabled", () => {
+        cy.request({
+            method: "POST",
+            url: "https://keycloak.admin.uds.dev/realms/uds/clients-registrations/default",
+            failOnStatusCode: false,
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: {
+                clientId: "dcr-should-be-disabled",
+                redirectUris: ["http://localhost"],
+                publicClient: true,
+            },
+        }).then((response) => {
+            expect([401, 403]).to.include(response.status);
         });
-
-        it("UDSClientPolicyPermissionsExecutor removes non-whitelisted mappers and claims", () => {
-            cy.exec("uds zarf tools kubectl apply -f ./resources/test-package.yaml").its('code').should('eq', 0);
-            cy.exec("uds zarf tools kubectl wait --for=condition=Ready package/test-package -n test-package --timeout=300s").its('code').should('eq', 0);
-
-            cy.getAccessToken().then((accessToken: string) => {
-                cy.request({
-                    method: 'GET',
-                    url: 'https://keycloak.admin.uds.dev/admin/realms/uds/clients',
-                    headers: {
-                        'Authorization': `Bearer ${accessToken}`,
-                        'Content-Type': 'application/json'
-                    },
-                }).then((response) => {
-                    expect(response.status).to.eq(200);
-                    const testClient = response.body.find((client: any) => client.clientId === "uds-core-admin-test");
-                    const protocolMappersUrl = `https://keycloak.admin.uds.dev/admin/realms/uds/clients/${testClient.id}/protocol-mappers/models`;
-                    cy.request({
-                        method: 'GET',
-                        url: protocolMappersUrl,
-                        headers: {
-                            'Authorization': `Bearer ${accessToken}`,
-                            'Content-Type': 'application/json'
-                        },
-                    }).then((response) => {
-                        expect(response.status).to.eq(200);
-
-                        interface ProtocolMapper {
-                            name: string;
-                        }
-                        const protocolMappers = response.body as ProtocolMapper[];
-                        expect(protocolMappers).to.have.length(3);
-                        expect(protocolMappers.map((mapper) => mapper.name)).to.include.members([
-                            "valid-protocol-mapper-1",
-                            "valid-protocol-mapper-2",
-                            "valid-protocol-mapper-3",
-                        ]);
-                    });
-                });
-            });
-        });
-    }
+    });
 });
