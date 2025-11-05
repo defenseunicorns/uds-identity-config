807b4ded61b5ac0a8bfea8ef0fefedccd73cc87e
UnicornChance <chance@defenseunicorns.com>
2025-11-05 13:49:15 -0700

    add additional e2e test

--- src/test/cypress/e2e/group-authz.cy.ts
+++ src/test/cypress/e2e/group-authz.cy.ts
@@ -58,4 +58,40 @@ describe('Group Authorization', () => {
     // Matches uds_access_denied_client in messages_en.properties
     cy.contains('p.instruction', 'You do not have access to this application. Please contact your administrator.').should('be.visible');
   });
+
+  it('Repeated group denials do not disable the account', () => {
+    const formData: RegistrationFormData = {
+      username: 'testing_user_no_groups',
+      password: 'Testingpassword1!!',
+    };
+
+    // Attempt 1: denied by group authorization
+    cy.accessGrafana();
+    cy.loginUser(formData.username, formData.password);
+    cy.url().should('include', 'https://sso.uds.dev');
+    cy.contains('p.instruction', 'You do not have access to this application. Please contact your administrator.')
+      .should('be.visible');
+
+    // Attempt 2: denied again
+    cy.accessGrafana();
+    cy.loginUser(formData.username, formData.password);
+    cy.url().should('include', 'https://sso.uds.dev');
+    cy.contains('p.instruction', 'You do not have access to this application. Please contact your administrator.')
+      .should('be.visible');
+
+    // Attempt 3: denied again
+    cy.accessGrafana();
+    cy.loginUser(formData.username, formData.password);
+    cy.url().should('include', 'https://sso.uds.dev');
+    cy.contains('p.instruction', 'You do not have access to this application. Please contact your administrator.')
+      .should('be.visible');
+
+    // Prove account is not disabled/locked by logging into an allowed client (account console)
+    cy.visit('https://sso.uds.dev/realms/uds/account/');
+    cy.loginUser(formData.username, formData.password);
+    // Confirm we are on the account console and the account is usable
+    cy.url().should('include', '/realms/uds/account/');
+    // Look for common tabs present in the Keycloak account console
+    cy.contains(/Personal info|Account security|Applications|Sessions/i).should('be.visible');
+  });
 });
