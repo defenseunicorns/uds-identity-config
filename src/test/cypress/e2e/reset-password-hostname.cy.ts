c60ab7c6ee760ca5068a3e7f5178a4b4e4655cff
UnicornChance <chance@defenseunicorns.com>
2026-08-19 14:34:30 -0700

    test: decouple reset URL coverage from Core admin host

--- src/test/cypress/e2e/reset-password-hostname.cy.ts
+++ src/test/cypress/e2e/reset-password-hostname.cy.ts
@@ -4,20 +4,17 @@
  */
 
 describe("Reset Password URL", () => {
-  ["https://sso.uds.dev", "https://keycloak.admin.uds.dev"].forEach(loginHost => {
-    it(`uses the public SSO hostname when loaded from ${loginHost}`, () => {
-      const params = new URLSearchParams({
-        client_id: "account",
-        redirect_uri: "https://sso.uds.dev/realms/uds/account/",
-        response_type: "code",
-        scope: "openid",
-      });
-
-      cy.visit(`${loginHost}/realms/uds/protocol/openid-connect/auth?${params.toString()}`);
-      cy.location("host").should("eq", new URL(loginHost).host);
-      cy.get('a[href*="/login-actions/reset-credentials"]')
-        .should("have.attr", "href")
-        .and("match", /^https:\/\/sso\.uds\.dev\/realms\/uds\/login-actions\/reset-credentials\?/);
+  it("uses the public SSO hostname", () => {
+    const params = new URLSearchParams({
+      client_id: "account",
+      redirect_uri: "https://sso.uds.dev/realms/uds/account/",
+      response_type: "code",
+      scope: "openid",
     });
+
+    cy.visit(`https://sso.uds.dev/realms/uds/protocol/openid-connect/auth?${params.toString()}`);
+    cy.get('a[href*="/login-actions/reset-credentials"]')
+      .should("have.attr", "href")
+      .and("match", /^https:\/\/sso\.uds\.dev\/realms\/uds\/login-actions\/reset-credentials\?/);
   });
 });
